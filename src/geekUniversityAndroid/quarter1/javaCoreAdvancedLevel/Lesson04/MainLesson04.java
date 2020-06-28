d479c4c96432e440626128cbd1dea9a6f3dbf5d7
Vladimir Svishch <5693031@gmail.com>
2020-06-28 11:51:08 +0300

    hw-2-05

---
+++ src/geekUniversityAndroid/quarter1/javaCoreAdvancedLevel/Lesson04/MainLesson04.java
@@ -0,0 +1,11 @@
+package geekUniversityAndroid.quarter1.javaCoreAdvancedLevel.Lesson04;
+    /*
+        ФИО:  		    Свищ Владимир Сергеевич
+        Факультет:      Geek University Android-разработки
+        Курс: 		    Java Core. Продвинутый уровень
+        Урок: 		    Урок 4. Продвинутые вопросы создания графического интерфейса
+        Дата правки:    25.06.2020
+        Git:            https://github.com/BikerIndian/geekbrains_JavaFX/pull/1
+    */
+public class MainLesson04 {
+}
