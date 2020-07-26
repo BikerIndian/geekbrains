956c6a29bb2d75f1e225ff4dad646b0b478c42e9
Vladimir Svishch <5693031@gmail.com>
2020-07-26 21:05:46 +0300

    hw-alg-03 1. Задача

--- src/geekUniversityAndroid/alg/lesson03/MainLesson03.java
+++ src/geekUniversityAndroid/alg/lesson03/MainLesson03.java
@@ -8,7 +8,43 @@ package geekUniversityAndroid.alg.lesson03;
 */
 public class MainLesson03 {
     public static void main(String[] args) {
+        /*
+        1. Создать программу, которая переворачивает
+        вводимые строки (читает справа налево).
+        */
+       // printReversStr("Java");
 
+        /*
+        2. Создать класс для реализации дека.
+         */
+        testDeca();
+    }
+
+
+
+    private static void printReversStr(String java) {
+        char[] charArr = java.toCharArray();
+        MyStack<Character> stack= new MyStack<>();
+        for (int i = 0; i < charArr.length; i++) {
+            stack.push(charArr[i]);
+        }
+
+        for (int i = 0; i < charArr.length; i++) {
+            System.out.print(stack.pop());
+        }
+    }
+
+    private static void testDeca() {
+        MyDeca<Integer> deca = new MyDeca<>();
+
+        deca.InsertRight(1);
+        deca.InsertRight(2);
+        deca.InsertRight(3);
+
+
+        System.out.println(deca);
+        System.out.println(deca.removeLeft());
+        System.out.println(deca);
     }
 }
 
