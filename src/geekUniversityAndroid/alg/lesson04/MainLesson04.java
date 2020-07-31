bc18fb08e2373ee3c1486b8f9469deed555a46ed
Vladimir Svishch <5693031@gmail.com>
2020-07-31 11:47:41 +0300

    hw-alg-04

---
+++ src/geekUniversityAndroid/alg/lesson04/MainLesson04.java
@@ -0,0 +1,26 @@
+package geekUniversityAndroid.alg.lesson04;
+
+/*
+      ФИО:  		    Свищ Владимир Сергеевич
+      Факультет:        Geek University Android-разработки
+      Курс: 		    Алгоритмы и структуры данных на Java.
+      Урок: 		    Урок 4. Связанные списки
+      Дата правки:      31.07.2020
+*/
+
+public class MainLesson04 {
+    public static void main(String[] args) {
+        
+        // 1. Добавить ListIterator, реализовать некоторые из его методов.
+        testListIterator();
+        // 2. Реализовать LinkedQueue (очередь, на базе связанных списков.)
+        testLinkedQueue();
+        
+    }
+
+    private static void testListIterator() {
+    }
+
+    private static void testLinkedQueue() {
+    }
+}
