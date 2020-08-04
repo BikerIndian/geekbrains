d73109c15707d5a6aede7323ac9b43c2856ed06c
Vladimir Svishch <5693031@gmail.com>
2020-08-04 16:46:03 +0300

    hw-alg-05

---
+++ src/geekUniversityAndroid/alg/lesson05/MainLesson05.java
@@ -0,0 +1,34 @@
+package geekUniversityAndroid.alg.lesson05;
+
+public class MainLesson05 {
+
+    /*
+      ФИО:  		    Свищ Владимир Сергеевич
+      Факультет:        Geek University Android-разработки
+      Курс: 		    Алгоритмы и структуры данных на Java.
+      Урок: 		    Урок 5. Рекурсия
+      Дата правки:      04.08.2020
+    */
+
+    public static void main(String[] args) {
+        /*
+        1. Написать программу по возведению числа в степень с помощью рекурсии.
+         */
+        degree();
+
+        /*
+        2. Написать программу «Задача о рюкзаке» с помощью рекурсии.
+         */
+        backpack();
+
+    }
+
+    private static void degree() {
+
+    }
+
+    private static void backpack() {
+
+    }
+
+}
