d12e2e8b1e7c22e705f482ac4b01f94800b1318d
Vladimir Svishch <5693031@gmail.com>
2020-08-13 11:44:39 +0300

    hw-alg-08 1. Задание

--- src/geekUniversityAndroid/alg/lesson08/MainLesson08.java
+++ src/geekUniversityAndroid/alg/lesson08/MainLesson08.java
@@ -1,5 +1,8 @@
 package geekUniversityAndroid.alg.lesson08;
 
+import java.util.HashMap;
+import java.util.Random;
+
 public class MainLesson08 {
 
     /*
@@ -12,18 +15,27 @@ public class MainLesson08 {
 
     public static void main(String[] args) {
 
+
         ChainingHashMap<Integer, String> chm = new ChainingHashMap<>();
         chm.put(1,"one");
         chm.put(2,"two");
         chm.put(3,"three");
+        chm.put(59,"three");
         chm.put(4,"four");
         chm.put(5,"five");
 
         System.out.println(chm.get(3));
         System.out.println(chm);
 
+
         /*
         1. Создать метод удаления в классе метод цепочек.
          */
+
+
+        System.out.println("remove = " + chm.remove(3));
+        System.out.println(chm);
+
+
     }
 }
